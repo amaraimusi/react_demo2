b4a00942758b408a351021b980f3f2576afb79a3
amaraimusi <amaraimusi@gmail.com>
2023-10-06 23:26:49 +0900

    ReactとTypeScriptの最低限な見本

---
+++ dev/resources/ts/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+  return (
+	<div>Hello World! 今日の天気はいかがですか？</div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('react_app'));
--- dev/resources/views/welcome.blade.php
+++ dev/resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        @viteReactRefresh
+        @vite('resources/ts/index.tsx')
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,6 +18,8 @@
         </style>
     </head>
     <body class="antialiased">
+    	<div id="react_app"></div><!-- Reactの埋め込み部分 -->
+    	
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
