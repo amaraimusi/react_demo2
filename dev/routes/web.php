36435b8b758cfc11515386f0c70b0f60f75df474
amaraimusi <amaraimusi@gmail.com>
2023-10-07 21:48:45 +0900

    phpMyAdminを使えるようにする

--- dev/routes/web.php
+++ dev/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('demo', 'App\Http\Controllers\DemoController@index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
